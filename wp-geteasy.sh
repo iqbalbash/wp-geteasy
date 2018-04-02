c616c2669d70b1f6bde0dd433af945f821bb41c5
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 04:18:13 +0700

    update

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -10,6 +10,6 @@ unzip latest.zip
 cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
 
 Menghapus berkas yang tidak terpakai
-cd ../ && rm -rf wordpress wp-geteasy.sh
+cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
 
 ## Sekarang buka domain.tld dibrowser
