eef0e02325c4c7016e0c6eb0e467a15e26641a30
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 04:03:42 +0700

    first commit

---
+++ wp-geteasy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Mendownload WordPress terbaru
+wget https://wordpress.org/latest.zip
+
+## Mengekstrak WordPress
+unzip latest.zip
+
+## Memindahkan file ke folder root
+mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
+
+Menghapus berkas yang tidak terpakai
+cd ../ && rm -rf wordpress
+
+## Sekarang buka domain.tld dibrowser
