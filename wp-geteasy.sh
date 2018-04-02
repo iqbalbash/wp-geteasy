aadad1a6ba4bfeda800866be16b58bf53b3c1f42
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 04:13:29 +0700

    update

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -7,7 +7,7 @@ wget https://wordpress.org/latest.zip
 unzip latest.zip
 
 ## Memindahkan file ke folder root
-mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
+cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
 
 Menghapus berkas yang tidak terpakai
 cd ../ && rm -rf wordpress
