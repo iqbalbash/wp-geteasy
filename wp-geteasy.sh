21566b96024b36f76b5d1563790822068249b879
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 15:21:04 +0700

    perjelas sh

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Mendownload WordPress terbaru"
-	wget https://wordpress.org/latest.zip
+wget https://wordpress.org/latest.zip
 echo "Mengekstrak WordPress"
-	unzip latest.zip
+unzip latest.zip
 echo "Memindahkan file ke directori $PWD"
-	cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
+cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
 echo "Menghapus berkas yang tidak terpakai"
-	cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
-echo "Sekarang di browser untuk konfigurasi selanjutnya"
+cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
+echo "Sekarang buka browser untuk konfigurasi selanjutnya"
