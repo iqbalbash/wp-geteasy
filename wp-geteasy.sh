fef000b4c154fc450afafb6db2109150823326b8
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 15:17:21 +0700

    update sh

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Mendownload WordPress terbaru"
 	wget https://wordpress.org/latest.zip
 echo "Mengekstrak WordPress"
@@ -8,3 +7,4 @@ echo "Memindahkan file ke directori $PWD"
 	cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
 echo "Menghapus berkas yang tidak terpakai"
 	cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
+echo "Sekarang di browser untuk konfigurasi selanjutnya"
