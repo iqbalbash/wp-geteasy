3d4cd8de3588dd1de3596cce40cc2f0017834aac
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 15:37:23 +0700

    monitor progress pada sh

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -6,15 +6,15 @@ wget https://wordpress.org/latest.zip
 
 ## Ekstrak File
 echo "Mengekstrak WordPress"
-unzip latest.zip
+pmonitor -c unzip latest.zip
 
 ## Memindahkan File
 echo "Memindahkan file ke directori $PWD"
-cd wordpress && mv index.php readme.html wp-admin wp-comments-post.php wp-content wp-includes wp-load.php wp-mail.php wp-signup.php xmlrpc.php license.txt  wp-activate.php wp-blog-header.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-login.php wp-settings.php  wp-trackback.php ../
+pmonitor -c cd wordpress && pmonitor -c mv index.php readme.html wp-admin wp-comments-post.php wp-content wp-includes wp-load.php wp-mail.php wp-signup.php xmlrpc.php license.txt  wp-activate.php wp-blog-header.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-login.php wp-settings.php  wp-trackback.php ../
 
 ## Menghapus File
 echo "Menghapus berkas yang tidak terpakai"
-cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
+cd ../ && pmonitor -c rm -rf wordpress latest.zip wp-geteasy.sh
 
 ## Konfigurasi di Browser
 echo "Sekarang buka browser untuk konfigurasi selanjutnya"
