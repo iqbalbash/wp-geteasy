a78cfc7ddc4b952a25a104ffd41291dc5e1431f5
iqbalbash <id.iqbalbash@gmail.com>
2018-04-03 15:31:23 +0700

    mengganti file yang salah

--- wp-geteasy.sh
+++ wp-geteasy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+## Download WordPress
 echo "Mendownload WordPress terbaru"
 wget https://wordpress.org/latest.zip
+
+## Ekstrak File
 echo "Mengekstrak WordPress"
 unzip latest.zip
+
+## Memindahkan File
 echo "Memindahkan file ke directori $PWD"
-cd wordpress && mv wp-admin wp-content wp-includes index.php license.txt readme.html wp-active.php wp-blog-header.php wp-comments-post.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-load.php wp-login.php wp-mail.php wp-settings.php wp-signup.php wp-trackback.php xmlrpc.php ../
+cd wordpress && mv index.php readme.html wp-admin wp-comments-post.php wp-content wp-includes wp-load.php wp-mail.php wp-signup.php xmlrpc.php license.txt  wp-activate.php wp-blog-header.php wp-config-sample.php wp-cron.php wp-links-opml.php wp-login.php wp-settings.php  wp-trackback.php ../
+
+## Menghapus File
 echo "Menghapus berkas yang tidak terpakai"
 cd ../ && rm -rf wordpress latest.zip wp-geteasy.sh
+
+## Konfigurasi di Browser
 echo "Sekarang buka browser untuk konfigurasi selanjutnya"
